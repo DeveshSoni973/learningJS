b3f08e3771db4c34014db90383d27acaf11471d1
Devesh Soni <94973383+DeveshSoni973@users.noreply.github.com>
2025-04-03 10:38:42 +0000

    scope of this and arrow functions

--- 16Functions.js
+++ 16Functions.js
@@ -84,17 +84,85 @@ if (user.username=="Nemo"){
 }
 console.log(user);
 
-// EXPRESSIONS
 
+
+// EXPRESSIONS
 
 function addtwo(num){
     return num+2
 }
 
-console.log(addtwo(5))
+console.log(addtwo(5));
 
-const add2=function(num){
-    return num+2
+const add2 = function(num){
+    return num+2;
 }
 
-console.log(add2(5))
+console.log(add2(5));
+
+// ARROW FUNCTIONS
+
+const addThree = (num) => num + 3;
+console.log(addThree(5)); // 8
+
+const multiply = (a, b) => a * b;
+console.log(multiply(3, 4)); // 12
+
+const greet = () => console.log("Hello");
+greet();
+
+// THIS SCOPE
+
+// 1. Global Scope
+console.log(this); 
+// Browser: Logs `Window`
+// Node.js: Logs `{}` (empty object in ES module, `global` in CommonJS)
+
+// 2. Inside a Regular Function
+function regularFunction() {
+    console.log(this);
+}
+regularFunction();
+// Browser: Logs `Window` (or `undefined` in strict mode)
+// Node.js: Logs `global` (or `undefined` in strict mode)
+
+// 3. Inside an Arrow Function
+const arrowFunction = () => {
+    console.log(this);
+};
+arrowFunction();
+// Always logs `Window` in browser or `{}` in Node.js (inherits `this` from surrounding scope)
+
+// 4. Inside an Object Method (Regular Function)
+const user = {
+    name: "Nemo",
+    showThis: function() {
+        console.log(this);
+    }
+};
+user.showThis(); 
+// Logs the `user` object because `this` refers to the object calling the method
+
+// 5. Inside an Object Method (Arrow Function)
+const userArrow = {
+    name: "Nemo",
+    showThis: () => {
+        console.log(this);
+    }
+};
+userArrow.showThis(); 
+// Logs `Window` (or `{}` in Node.js) because arrow functions don’t have their own `this`, they inherit from the surrounding scope
+
+// 6. Inside a Constructor Function
+function Person(name) {
+    this.name = name;
+    console.log(this);
+}
+const p = new Person("Xiao");
+// Logs the new `Person` object
+
+// 7. Inside an Event Listener
+document.body.addEventListener("click", function() {
+    console.log(this);
+});
+// Logs the element that was clicked (because `this` refers to the event target)
